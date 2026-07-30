da610ca809f3411d7ed22ef53b9243052e7d88af
dikshadamahe <dikshadamahe1@gmail.com>
2026-07-30 15:16:44 +0530

    Add publications data

---
+++ app/constants/publications.ts
@@ -0,0 +1,14 @@
+import { Publication } from '@app-types';
+
+export const PUBLICATIONS: Publication[] = [
+  {
+    id: 'sparta',
+    title: 'Secure Phishing Analysis and Robust Training Algorithm (SPARTA) for Phishing Website Detection',
+    authors: 'Diksha Damahe, Ajay Kumar Phulre',
+    venue: "IEEE International Students' Conference on Electrical, Electronics and Computer Science (SCEECS)",
+    year: '2026',
+    location: 'Bhopal, India',
+    highlight: '99.15% detection rate on PhishTank and UCI benchmarks',
+    doi: 'https://doi.org/10.1109/SCEECS68810.2026.11430134',
+  },
+];
