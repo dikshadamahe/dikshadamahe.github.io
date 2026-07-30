ed4ed8cb907530efea58a97d36f37888359589cd
dikshadamahe <dikshadamahe1@gmail.com>
2026-07-30 11:47:09 +0530

    Add monsoon color constants

---
+++ app/constants/colors.ts
@@ -0,0 +1,11 @@
+export const COLORS = {
+  mistyBlue: '#B4C7D9',
+  paleGrey: '#D4D8DC',
+  cloudyWhite: '#EEF0F2',
+  mutedBlueGrey: '#8FA3B5',
+  wetRoad: '#6B7B8D',
+  rainAccent: '#A3C1D4',
+  greenery: '#7BA086',
+  raincoatYellow: '#D4A017',
+  earthWarm: '#8B7355',
+} as const;
