9fc835b15d664d1b336d6e18a7195d28e4d0b03c
dikshadamahe <dikshadamahe1@gmail.com>
2026-08-03 11:49:55 +0530

    Fix Hero animation

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import CanvasWrapper from '@components/CanvasWrapper';
+
+export default function Home() {
+  return (
+    <div className="h-[100dvh] w-full overflow-hidden">
+      <CanvasWrapper />
+    </div>
+  );
+}
